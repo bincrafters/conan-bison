fe3fab28e29c2a95f6769de4dcc728adc245e998
ericLemanissier <eric.lemanissier@gmail.com>
2019-02-06 12:47:29 +0100

    remove secure_snprintf.patch
    it is integrated upstream in 3.0.5

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,6 @@ class BisonConan(ConanFile):
     license = "GPL-3.0-or-later"
     authors = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
-    exports_sources = ["secure_snprintf.patch"]
     settings = "os", "arch", "compiler", "build_type"
 
     def source(self):
@@ -30,8 +29,6 @@ class BisonConan(ConanFile):
         del self.settings.compiler.libcxx
 
     def build(self):
-        if self.settings.os == "Macos" and float(self.settings.compiler.version.value) >= 9.1:
-            tools.patch(base_path="sources", patch_file="secure_snprintf.patch")
         env_build = AutoToolsBuildEnvironment(self)
         env_build.fpic = True
         configure_args = ['--prefix=%s' % self.package_folder]
