480807f394ad73f169bc5a8d4777b3445cbd2a10
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-02-05 03:30:39 +0100

    Test bison before testing cmake integration

--- test_package_installer/conanfile.py
+++ test_package_installer/conanfile.py
@@ -1,11 +1,10 @@
-import os
 from conans import ConanFile, tools, CMake
-from six import StringIO
+import os
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake",
+    generators = "cmake"
 
     def build_requirements(self):
         if not tools.cross_building(self.settings):
@@ -13,22 +12,26 @@ class TestPackageConan(ConanFile):
                     tools.os_info.detect_windows_subsystem() != "msys2":
                 self.build_requires("msys2/20190524")
 
-    def build(self):
-        # verify CMake integration
-        if not tools.cross_building(self.settings):
-            cmake = CMake(self)
-            cmake.configure()
-            cmake.build()
+    @ property
+    def _mc_parser_source(self):
+        return os.path.join(self.source_folder, "mc_parser.yy")
 
-    def test(self):
+    def build(self):
         if not tools.cross_building(self.settings):
             # verify bison may run
             self.run("bison --version", run_environment=True)
             # verify yacc may run
             self.run("yacc --version", run_environment=True, win_bash=tools.os_info.is_windows)
             # verify bison may preprocess something
-            mc_parser = os.path.join(self.source_folder, "mc_parser.yy")
-            self.run("bison -d %s" % mc_parser, run_environment=True)
+            self.run("bison -d {}".format(self._mc_parser_source), run_environment=True)
+
+            # verify CMake integration
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
             # verify bison doesn't have hard-coded paths
             bison = tools.which("bison")
             if tools.which("strings") and tools.which("grep"):
@@ -38,4 +41,4 @@ class TestPackageConan(ConanFile):
                     raise Exception("bison has hard-coded paths to conan: %s" % output)
             # verify bison works without BISON_PKGDATADIR and M4 environment variables
             with tools.environment_append({"BISON_PKGDATADIR": None, "M4": None}):
-                self.run("bison -d %s" % mc_parser, run_environment=True)
+                self.run("bison -d {}".format(self._mc_parser_source), run_environment=True)
